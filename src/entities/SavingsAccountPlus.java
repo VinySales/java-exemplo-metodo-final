a3f606760f2974997f2c8a6872360e0ad86c9c83
VinySales <vinysalesreboucas@hotmail.com>
2023-02-10 02:03:53 -0300

    first commit

---
+++ src/entities/SavingsAccountPlus.java
@@ -0,0 +1,9 @@
+package entities;
+
+public class SavingsAccountPlus extends SavingsAccount{
+//	Método withdraw da SavingsAccount possui final e não pode ser sobreposto
+//	@Override
+//	public void withdraw(Double amount) {
+//		this.balance -= amount + 10;
+//	}
+}
